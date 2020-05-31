360c089769fbbc81b6264ed7ff05c3ac97d31c78
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-05-31 20:30:55 +0700

    Initial Commit :fire:

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Header() {
-  return <h1>Hosea Tirtajaya</h1>;
+  const firstName = "Hosea";
+  const lastName = "Tirtajaya";
+
+  // return <h1>{firstName + " " + lastName}</h1>; this or using es6 string
+
+  return <h1>{`${firstName} ${lastName}`}</h1>;
 }
 
 export default Header;
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,32 @@
 import React from "react";
 
 function MainContent() {
+  // Menghitung siang atau malam atau pagi
+  const h3color = {
+    backgroundColor: "#121212",
+    fontSize: "24px",
+  };
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+    h3color.color = "#04756F";
+    h3color.backgroundColor = "whitesmoke";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+    h3color.color = "Orange";
+  } else {
+    timeOfDay = "night";
+    h3color.color = "#121212";
+    h3color.backgroundColor = "whitesmoke";
+  }
+
   return (
     <div>
+      {/* <h3 style={{ color: "aquamarine" }}>Good {timeOfDay}!</h3> This can be get longer by the time we add more css*/}
+      <h3 style={h3color}>Good {timeOfDay}!</h3>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut
         eleifend urna, id malesuada ipsum. Quisque accumsan dolor nisl, vel
